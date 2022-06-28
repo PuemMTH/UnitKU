43af2337ab1670ba979cff197b08c31b46c41267
PuemMTH <puem.mth@gmail.com>
2022-06-28 17:16:27 +0700

    master

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,6 @@
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 import HomeDashboard from '@/components/HomeDashboard.vue'
 export default {
   name: 'HomeView',
